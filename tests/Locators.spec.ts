a8f0e84c4bc7a75391c976862db0381c78ea39a4
Jeannie Teo <jeannieteo78@gmail.com>
2025-07-07 12:22:27 +0800

    Update Locators.spec.ts
    change to use helper

--- tests/Locators.spec.ts
+++ tests/Locators.spec.ts
@@ -1,17 +1,11 @@
 import {test, expect} from "@playwright/test"
+import { loginDemoBlaze, logoutDemoBlaze } from "./helper";
 
 test('Locators', async ({page})=>{
     await page.goto('https://demoblaze.com/');
-    //await page.locator("id=login2").click() //property
-    await page.click("id=login2");
-    await page.fill("#loginusername", 'tester1@test.com');
-    await page.fill("#loginpassword", 'password@1234');
-    await page.click("//button[text()='Log in']");
-    //await page.getByRole('button', {type: 'submit'}).click();
+    await loginDemoBlaze(page);
     
-    
-
-    //locate mutliple
+    //locate mutltiple
     const elements = await page.$$("//div[@id='tbodyid']//h4//a");
     console.log("No of products found: " + elements.length);
     expect.soft(elements.length).toEqual(9); //soft assert
@@ -20,7 +14,7 @@ test('Locators', async ({page})=>{
         const textthis = await element.textContent();
         console.log("product: " + textthis);    
     }
-    const logoutlink = await page.locator("//a[text()='Log out']");
-    await expect(logoutlink).toBeVisible();
+    await logoutDemoBlaze(page);
+
     await page.close();
 })
